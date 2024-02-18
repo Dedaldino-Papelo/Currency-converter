4260b7434bf63ceeb107c0a77688b36e60e549b2
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2024-02-18 17:46:32 +0100

    aligned the layout margin in xml file and add the logic to convert in the main activity

--- app/src/main/java/com/example/linear_layout/MainActivity.kt
+++ app/src/main/java/com/example/linear_layout/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.linear_layout
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.linear_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        convert()
+    }
+
+    private fun convert() {
+        binding.buttonConverter.setOnClickListener {
+            val euros = binding.editValue.text.toString().trim()
+
+            if(euros.isNotEmpty()) {
+                val dollar = euros.toDouble() * 0.8
+                binding.textResult.text = "$dollar $"
+            } else {
+                Toast.makeText(applicationContext, "Invalid Number", Toast.LENGTH_SHORT)
+            }
+        }
     }
 }
